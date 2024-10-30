726f9f30f466f823a1fd064a979f522445fa0b78
alex909w <alex909w@gmail.com>
2024-10-30 15:58:31 -0600

    Saldo Inicial

--- js/login.js
+++ js/login.js
@@ -8,7 +8,8 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     const errorMessage = document.getElementById('error-message');
 
     if (enteredPin === correctPin) {
-        
+        // Limpiar el localStorage
+        localStorage.clear();
         // Guardar los datos del usuario en localStorage
         localStorage.setItem('userName', 'Ash Ketchum');
         localStorage.setItem('userAccount', '0987654321');
